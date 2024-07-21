52b237bd19ec7c384ccde9e97a141073db072a06
Denys Bokov <denys.bokov@jonasundderwolf.de>
2024-07-21 18:04:00 +0200

    refactor(inlineStyles): styles with ?inline using vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.scss'
+import inlineStyles from './App.scss?inline';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <style>
+        {inlineStyles}
+      </style>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
