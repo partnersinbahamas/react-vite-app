38cb93b46689310303701b71ffebef4cd280d0ff
Denys Bokov <denisbokov1703@gmail.com>
2024-07-22 20:09:58 +0200

    build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // publicDir: 'viteBuild', // allows to rename public folder on npm run build
+  build: {
+    outDir: 'build' // Ensure this matches the directory you're using in gh-pages
+  },
   plugins: [
     react(),
     Inspect(),
