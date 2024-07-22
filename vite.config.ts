6f3bb708c6908cf61189e9a935d1c206b7b5b56c
Denys Bokov <denys.bokov@jonasundderwolf.de>
2024-07-22 20:03:56 +0200

    feat: add comment to publicDir ability

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // publicDir: 'viteBuild', // allows to rename public folder on npm run build
   plugins: [
     react(),
     Inspect(),
