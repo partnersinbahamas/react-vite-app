c49cb968b28fbfb75d5f4813b3f9938ef9705147
Denys Bokov <denisbokov1703@gmail.com>
2024-07-27 16:04:57 +0200

    feat: using math function as an modules with vate function import.glob

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import inlineStyles from './App.scss?inline';
@@ -10,17 +10,45 @@ import userRaw from './utils/json/user.json?raw'; // returns a raw json object
 import meImageUrl from './assets/me-work.jpeg?url'; // return url path to image folder
 import meImage from './assets/me-work.jpeg';
 
+/*
+  eager: disable a dynamic behavior, so it call module from box;
+  import: 'your exported variable' | 'default': returns only exported variable;
+  as: 
+    - raw: returns module as a raw string;
+    - url: path to module;
+
+  ^ moudles params description
+  const mathModules = import.meta.glob('/src/mathFunctions/*.ts', { import: 'funcDescription', eager: true });
+*/
+
+const mathModules = import.meta.glob('/src/mathFunctions/*.ts', ); // eager disable a dynamic behavior, so it call modules from box
+
 function App() {
   const [count, setCount] = useState(0);
+  const [maths, setMaths] = useState<any[]>([]);
 
   console.log(styles);
   console.log(user, userRaw, userUrl);
+  console.log(mathModules);
+
+  useEffect(() => {
+    Object.values(mathModules)
+      .forEach((importModule) =>
+        importModule().then((module: any) =>
+          setMaths((current) => [...current, module.default(1, 2)])));
+  }, []);
 
   return (
     <>
       <style>
         {inlineStyles}
       </style>
+
+      <ul style={{ position: 'absolute', right: '10px' }}>
+        {maths.map((math) => (
+          <li>{math}</li>
+        ))}
+      </ul>
       <div>
         <a href="https://www.linkedin.com/in/denys-bokov-339b22277/" target="_blank">
           <img src={meImage} className="me" alt="Vite logo" />
