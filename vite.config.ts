7622d2c6a7f4505c38808ddde27c531cae1e2471
Denys Bokov <denisbokov1703@gmail.com>
2024-07-28 15:55:25 +0200

    feat: rename envPrefix from VITE_ to APP_

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  envPrefix: 'APP_',
   // publicDir: 'viteBuild', // allows to rename public folder on npm run build
   build: {
     outDir: 'build' // Ensure this matches the directory you're using in gh-pages
