74f02b4020abab627d27982fbeb31c436dbfa9f4
Denys Bokov <denisbokov1703@gmail.com>
2024-09-02 23:24:26 +0200

    feat(module.js): add hot.dispose

--- src/utils/js/module.js
+++ src/utils/js/module.js
@@ -5,9 +5,11 @@ export default 'default message from modules.js';
 
 console.log('log from module.js');
 
-if (import.meta.hot) {
+if (import.meta.hot) { // meta.hot update js files w/o page reload.
   import.meta.hot.accept(['./submodule.js'], ([newSubmodule, newModule]) => {
     console.log('[vite]:update module.js', newModule)
     console.log('[vite]:update submodule.js', newSubmodule)
   })
 }
+
+import.meta.hot.dispose(() => {}) // use to clean-up any persistent side effects created by meta.hot.accept:
